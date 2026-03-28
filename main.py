26ec9eb79044d0b570fdafd9980d7b1fd796b404
l2kgamingofficial277-netizen <l2kgamingofficial277@gmail.com>
2026-03-28 20:48:39 +0600

    main.py

--- main.py
+++ main.py
@@ -267,5 +267,3 @@ if __name__ == '__main__':
     
     # Run Flask app
     app.run(host='0.0.0.0', port=port, debug=False)
-    if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
